2bd15d939acd02c9a542f7db3454e95013934b03
Dimitar Nestorov <dimitar.nestorov@callstack.com>
2023-04-11 17:55:12 +0300

    Install React Navigation and add a simple tabs navigator

---
+++ src/Profile.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default function Profile() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Profile Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  text: {
+    textAlign: 'center',
+  },
+});
